131d3e5fd97929b4a16dfa864aae75bbefe36137
Yana <yanacodliveroil@gmail.com>
2025-07-17 11:25:49 +1000

    подключила временно к mongo db
    пробуем чесночную шаурму .. оп оп оп .. и еще чуть чуть .. оп оп

--- server/index.js
+++ server/index.js
@@ -1,15 +1,32 @@
-const express = require('express') 
-const app = express()
-const port = 3000
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+import userRoute from "./routes/user-route.js";
+import productRoute from "./routes/product-route.js";
+import orderRoute from "./routes/order-route.js";
+
+const app = express();
+app.use(bodyParser.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const MONGODBURL = process.env.MONGODB_URL;
+
+mongoose.connect(MONGODBURL).then(() => {
+    console.log("Database connected successfully!");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port: ${PORT}`);
+    })
+}).catch(error => console.log(error));
+
+app.use("/api/user", userRoute);
+app.use("/api/product", productRoute);
+app.use("/api/order", orderRoute);
+
+//const express = require('express') 
+
 //import cors from 'cors';
 //app.use(cors());
-
-
-app.get('/', (req, res) => {
-    res.send('HI')
-})
-
-app.listen(port, () => {
-    console.log('smth')
-})
-
--- server/routes/order-route.js
+++ server/routes/order-route.js
@@ -1,7 +1,13 @@
 import express from "express";
-import {create, fetchOrders, fetchActiveOrders, fetchActiveOrders, update, deleteOrder} from "../controllers/order-controller.js";
+import {create, fetchOrders, fetchActiveOrders, fetchArchivedOrders, update, deleteOrder} from "../controllers/order-controller.js";
 
 const route = express.Router();
 
 route.post("/create", create);
-route.get("/getallorders", fetchOrders)
+route.get("/getallorders", fetchOrders);
+route.get("/getactiveorders", fetchActiveOrders);
+route.get("/getarchivedorders", fetchArchivedOrders);
+route.put("/update/:id", update);
+route.delete("/delete/:id", deleteOrder);
+
+export default route;
